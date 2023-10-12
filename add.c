6a2a1d640bc941740c72e77a52ed5c2985703593
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-12 20:25:37 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,21 @@
+// write a program of adding two numbers.
+/************************************
+ statement = add two number
+ programmer name = taniya yadav
+ *********************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter number a\n");
+    scanf("%d",&a);
+
+    printf("enter number b\n");
+    scanf("%d",&b);
+
+     printf("the sum is %d\n",a+b);
+
+    return 0;
+  
+}
