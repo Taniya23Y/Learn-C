689817d6adfabc9469c6b5e3f5be1eb1e883f931
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-13 00:45:30 +0530

    Add files via upload

---
+++ 01)airthemeticoperator1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a;
+    float b;
+    a = 34;
+    b = 6.34;
+
+    printf("a + b =%f\n",a+b);
+    printf("a - b =%f\n",a-b);
+    printf("a * b =%f\n",a*b);
+    printf("a / b =%f\n",a/b);
+
+    return 0;
+}
