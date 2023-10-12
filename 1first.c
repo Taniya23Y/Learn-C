c80058531910c89a56503f9e20c6673981965942
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-12 20:20:56 +0530

    Add files via upload

---
+++ 1first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+// void main()
+int main()
+{
+    int num;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    printf("%d" , num);
+    // getch();
+   return 0;
+}
