7845dbf1f426e8849039c5ae2e5aba7734fa47f5
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-14 13:14:30 +0530

    Add files via upload

---
+++ 13)decrementoperator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a =5;
+
+    printf("a:%d\n",a);
+    printf("a:%d\n",--a);
+    printf("a:%d\n",a);
+    printf("a:%d\n",a--);
+    printf("a:%d\n",a);
+
+    return 0;
+}
