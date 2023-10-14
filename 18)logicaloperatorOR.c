4e70e8e904ae2cbd8a352ebb091fc6520f8df530
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-14 13:17:42 +0530

    Add files via upload

---
+++ 18)logicaloperatorOR.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int x , y;
+    x = 57;
+    y = 34;
+    /*
+    int x,y;
+    x= 0;
+    y=0;
+    */
+
+    printf("x||y:%d\n",x||y);
+    return 0;
+}
