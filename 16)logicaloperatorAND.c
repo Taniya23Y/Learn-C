825a8e41c1d55e6ec92cdcb57ec10841e0a21d50
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-14 13:16:10 +0530

    Add files via upload

---
+++ 16)logicaloperatorAND.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int x=34,y=57;
+    int z;
+    z=(x>5)&&(y<80);
+
+    printf("x&&y:%d\n",z);
+
+
+    return 0;
+}
