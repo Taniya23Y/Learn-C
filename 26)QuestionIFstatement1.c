e88110cd110e606d4e08c49edf72040609a3e145
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-15 22:23:48 +0530

    Add files via upload

---
+++ 26)QuestionIFstatement1.c
@@ -0,0 +1,30 @@
+/* write a program of if statement ...while purchasing certain items , a discount of 10%
+ is offered if the quantity purchased is more than 1000.if the quantity and price per item 
+ are iput through keyboard , write a program to calculate the total expenses. */
+
+ /**********************************************************
+  * statement = calculate the total expenses
+  * programmer name = taniya yadav
+*********************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int qty,dis=0;
+    float rate,tot;
+
+    // taking input from user
+    printf("Enter quatity,rate\n");
+    scanf("%d %f", &qty, &rate);
+
+    if (qty>1000) /*(condition)*/
+    {
+        /* code/ execution statement */
+        dis = 10;
+        tot = (qty*rate)-(qty*rate*dis/100);
+        printf("total expenses=%f",tot);
+
+    }
+      return 0;    
+}
