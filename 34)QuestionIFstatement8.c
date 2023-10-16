b898ba2a48523b58fb232aa1c0556be42c0325e5
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-16 12:45:32 +0530

    Add files via upload

---
+++ 34)QuestionIFstatement8.c
@@ -0,0 +1,38 @@
+/*write a program that accepts a three digit number number from user and check wheather
+ it is Armstrong or not.*/
+/*****************************************************************
+ * statement = checking armstrong or not (1634,153,407)
+ * progammer name = taniya yadav
+******************************************************************/
+
+#include "stdio.h"
+int main()
+{
+   int num, originalNum, remainder, result=0;
+   printf("ENTER a three digit number:\n");
+   scanf("%d", &num);
+
+     originalNum=num;
+
+    while(originalNum!=0)
+    {
+      // remainder contain the last digit
+      remainder = originalNum % 10 ;
+      result += remainder*remainder*remainder;
+
+      // moving the last digit to the original digit
+      originalNum /= 10;
+
+    }
+    // taking condition on if statement
+   if(result == num)
+   {
+       printf(" %d three digit number is a Armstrong number.", num);
+   }
+   else
+   {
+    printf("%d three digit number is not a Armstrong number.", num);
+   }
+
+return 0;
+}
