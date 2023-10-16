9b12ef30533158a23df339b8f9ebbd4dc4cb907d
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-16 12:42:27 +0530

    Add files via upload

---
+++ 30)QuestionIFstatement5.c
@@ -0,0 +1,33 @@
+/* write a program that accepts a number from user and check whaether it is positive 
+ or negavtive or zero. */
+
+/***********************************************************
+ * statement = if else / if statement
+ * programmer name = taniya yadav
+ ************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int num;
+
+    //  taking input from user
+    printf("Enter the number:\n");
+    scanf("%d", &num);
+
+    if (num>0)
+    {
+      printf("%d is a positive number.",num); 
+    }
+      else if(num<0)
+       {
+           printf("%d is a negative number.",num);
+       }
+        else if(num==0)
+          {
+            printf(" %d is a zero.",num);
+          }
+    return 0;
+}
