7cc079e9920b7f4adfcdb4f44e739ecc6f895b71
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-18 17:39:10 +0530

    Add files via upload

---
+++ 58)QuestionOperator1.c
@@ -0,0 +1,19 @@
+// write a program to calculate square of a given number?
+/*******************************************************
+ statement = square of no. 
+ programmer name = taniya yadav
+*******************************************************/
+
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a,b;
+    a =78;
+    b = a*a;
+
+    printf("the square of given number a:%d\n",b);
+    return 0;
+}
