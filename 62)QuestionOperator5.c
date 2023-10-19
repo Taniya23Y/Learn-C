faee7bbbd2dd848bed55b57347919ef553b49811
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-19 08:54:07 +0530

    Add files via upload

---
+++ 62)QuestionOperator5.c
@@ -0,0 +1,19 @@
+// wite a program to calculate area of triangle?
+/**********************************************
+ statement = area of triangle
+ programmer name = taniya yadav
+*********************************************/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a,b,c;
+    a = 25;
+    b = 45;
+    c = (a+b)/2;
+
+   printf("the area of triangle: %d\n",c);
+   return 0;
+}
