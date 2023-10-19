f7f811860d036dd02b0f1b4285f314f1462e5df8
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-19 08:54:27 +0530

    Add files via upload

---
+++ 63)QuestionOperator6.c
@@ -0,0 +1,19 @@
+// wriyte a program to calculate area of circle?
+/**********************************************
+ statement = area of circle
+ programmer name = taniya yadav
+**********************************************/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    float pie,r,A;
+    pie = 3.14;
+    r = 7;
+    A = (pie*(r*r));
+
+    printf("Area if circle: %f\n",A);
+    return 0;
+}
