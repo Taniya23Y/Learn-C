a901f02340e93930700e40308bac6ef160a2b92f
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-22 11:03:47 +0530

    Add files via upload

---
+++ 79)QuestionOperator22.c
@@ -0,0 +1,28 @@
+/*If a five-digit number is input through the keyboard, write a 
+program to calculate the sum of its digits. 
+(Hint: Use the modulus operator ‘%’)? */
+/*******************************************************************
+ statement = sum of given digit
+ programmer name = taniya yadav
+********************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+
+ int num,a,b,c,d,e;
+
+	printf("Enter a five digit number : ");
+	scanf("%d", &num);
+
+	e=num%10;
+	d=(num/10)%10;
+	c=(num/100)%10;
+	b=(num/1000)%10;
+	a=(num/10000);
+
+	printf("%d is the sum of the digits of the number %d.", a+b+c+d+e, num);
+	return 0;
+
+}
