4f748c6d29b3a9f6feae7866fc59d6c9b6c6f133
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-23 12:03:40 +0530

    Add files via upload

---
+++ 93)relationoperator1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int total=10,i=90;
+    total+=i;
+    //this is same as total+i
+    printf("total=%d\n",total);
+    return 0;
+}
