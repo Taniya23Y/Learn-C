378e8d1f3619e315d8197874ec07652730df9f2a
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-23 12:03:23 +0530

    Add files via upload

---
+++ 92)relationoperator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a,b;
+    a = 34;
+    b = 34;
+
+    printf("%d\n",a==b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a<=b);
+    printf("%d\n",a!=b);
+    printf("%d\n",a<b);
+    printf("%d\n",a>b);
+
+
+    return 0;
+}
