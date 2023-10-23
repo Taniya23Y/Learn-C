43cf679b5869566e56d9e21752a383d37b28196c
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-23 12:02:41 +0530

    Add files via upload

---
+++ 90)QuestionOperator33.c
@@ -0,0 +1,16 @@
+
+/**************************************************************************
+ statement = foramt specifier
+ programmer name = taniya yadav 
+*************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int a = 8;
+  float b = 7.333;
+  printf("%d the value of a is %d\n and the value of b is %f\n", a,b);
+
+    return 0;
+}
