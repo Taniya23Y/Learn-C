9a45f3ea7d8e5415c56329ccfb538a601fc79670
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-23 12:02:23 +0530

    Add files via upload

---
+++ 89)QuestionOperator32ASCII.c
@@ -0,0 +1,17 @@
+// print ASCII values
+
+/**********************************************************
+ * statement = ASCII value
+ * programmer name = taniya yadav
+************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  int i;
+  for(i=0;i<=255;i++)
+
+   printf("\ncharacter=%c and ASCII value=%d",i,i);
+   return 0;
+}
