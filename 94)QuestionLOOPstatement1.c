ff3a8edb34fa4305383c5a3a843322a0d7504702
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-24 17:03:25 +0530

    Add files via upload

---
+++ 94)QuestionLOOPstatement1.c
@@ -0,0 +1,80 @@
+// For understanding purpose.(while loop)
+
+#include <stdio.h>
+
+int main()
+/*
+{
+ int p, n, count; 
+ float r, si;
+
+ count = 1;
+ while ( count <= 3 )
+    {
+           printf ( "\nEnter values of p, n and r:\n " );
+           scanf("%d %d %f", &p, &n, &r);
+           si = p*r*n/100;
+           printf("simple interest = rs.%f",si);
+
+           count =count+1;
+    }
+return 0;
+}
+*/
+
+/*
+{
+    int a=10;
+    while (a<20)
+    {
+        printf("value of a:%d\n", a);
+        a++;
+        //a=a+1;
+    }
+return 0;
+}
+*/
+/*
+{
+    // semicolon at the end of while loop
+    int num=300;
+    while (num>255);// note it carefully      output is it doesn't print anything.
+     
+        printf("tanu uff tania\n");
+    
+    return 0;
+}
+*/
+/*
+{
+
+    // non zero number as a parameter
+
+    while(1)
+    printf("hello");
+    return 0;
+}
+*/
+
+
+{
+    // subscript variable remains same
+
+    int num =20;
+    while (num>10)
+    {
+        printf("hello");
+    }
+    return 0;
+}
+
+ 
+ /*
+{
+
+    // character as a parameter in hgile loop
+    while ('A')
+    printf("hello");
+    
+}
+*/
