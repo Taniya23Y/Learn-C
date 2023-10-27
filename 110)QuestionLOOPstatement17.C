3b48e1fa15e158414040aef68481b633dce5027f
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-10-27 21:03:59 +0530

    Add files via upload

---
+++ 110)QuestionLOOPstatement17.C
@@ -0,0 +1,113 @@
+// exercise questions from (yashwant kanethkar [5th edition book]).
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+   /*
+    int j;
+    while (j<=10)
+    {
+        printf("\n %d", j);       question = 1
+        j = j+1;
+    }
+    */
+
+   /*int i=1;
+    while (i<=10)
+    {
+        printf("\n %d", i);        question = 2
+        i =i+1;
+    }
+    */
+
+    /* int x=1;
+    while (x==1)
+    {
+        x = x - 1;
+        printf("\n %d", x);          question = 3
+    }
+    */
+
+   /*int x=1;
+    while (x==1)
+        x = x - 1;
+        printf("\n %d", x);       question = 4
+    */
+
+   /*char x;
+   while(x=0; x<=255; x++)
+   
+    printf("\nAscii value %d Character %c", x, x);   question = 5
+   
+   */
+     
+    /*int x = 4, y, z; 
+    y = --x; 
+    z = x--; 
+    printf("\n%d %d %d", x, y, z);         question = 6
+    */
+
+    /*int x = 4, y = 3, z; 
+    z = x-- -y; 
+    printf("\n%d %d %d", x, y, z);       question = 7
+    */
+
+    /*while('a' < 'b') 
+    printf("\nmalyalam is a palindrome");    question = 8
+    */
+
+    /*int i = 10; 
+    while( i = 20 ) 
+    printf("\nA computer buff!");       question = 9
+    */
+
+    /*int i; 
+    while( i = 10 ) 
+    {
+    printf("\n%d", i);          question = 10
+    i = i + 1; 
+    }
+    */
+
+    /*float x = 1.1 ; 
+    while ( x == 1.1 ) 
+    { 
+       printf("\n%f", x);      question = 11 
+       x = x – 0.1;  
+    }
+    */
+
+    /*while ( '1' < '2' ) 
+    printf ( "\nIn while loop" );   question = 12
+    */
+
+    /*char x ; 
+    for(x = 0; x <= 255; x++) 
+    printf("\nAscii value %d Character %c", x, x);   question = 13
+    */
+
+    /*int x = 4, y = 0, z; 
+    while (x >= 0) 
+    {                 output is 3 1
+       x--;                     1 3
+       y++;                     0 4
+       if (x == y)              -1 5
+       continue;   
+       else 
+       printf("\n%d %d", x, y);    question = 14
+    }
+    */
+
+    /*int x = 4, y = 0, z; 
+    while ( x >= 0 ) 
+    { 
+      if ( x == y )           output is 4 0
+      break;                            3 1 
+      else 
+      printf("\n%d %d", x, y);       question = 15
+      x--; 
+      y++; 
+    }  
+    */
+    return 0;
+}
