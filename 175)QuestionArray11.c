52c77c179bc85a5b386ede558968f0ccb103ab78
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-11-05 22:18:27 +0530

    Add files via upload

---
+++ 175)QuestionArray11.c
@@ -0,0 +1,26 @@
+//Assignment questions in c
+// Arrays in c
+/*write a program to sort array in descending order*/
+/****************************************************
+ * statement = sort array in descending order
+ * programmer name = taniya yadav
+****************************************************/
+
+#include <stdalign.h>
+int main()
+{
+    int array[100], num, i;
+    // taking input from user
+    printf("Enter number of element:\n");
+    scanf("%d", &num);
+    
+    printf("Enter %d integer\n", n);
+    
+    // 1st for loop
+    for ( i = 0; i < n; i++)
+    {
+       scanf()
+    }
+    
+    return 0;
+}
