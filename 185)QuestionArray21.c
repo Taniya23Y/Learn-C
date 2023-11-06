6937bc22e07cb83e67047f4d58d7171149e80cbc
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-11-06 17:44:24 +0530

    Add files via upload

---
+++ 185)QuestionArray21.c
@@ -0,0 +1,20 @@
+/*Passing array element in function*/
+/*call by addresss*/
+#include <stdio.h>
+int display(int *m);
+int main(int argc, char const *argv[])
+{
+    int i;
+    int marks[] = {50,56,78,90,34,23};
+    // loop
+    for (int i = 0; i < 6; i++)
+    {
+        display(&marks[i]);
+    }
+    
+    return 0;
+}
+
+int display(int *m){
+    printf("%d ", *m);
+}
