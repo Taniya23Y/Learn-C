645c111105edd115f21885632ca9a4f7396ec726
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-11-14 00:37:12 +0530

    Add files via upload

---
+++ 239)string20.c
@@ -0,0 +1,23 @@
+//  question  
+/*write a program to calculate length of the string*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+    char c[50] = "Taniya yadav";
+    printf("The string: %s", c);
+    printf("\nThe length of the string: %d", strlen(c));
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
